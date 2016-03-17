2d36eb6972f76f077d983c7dc5d8407874b69b25
Yue Cao <i@caoyue.me>
2016-03-17 18:18:16 +0800

    fix facebook avatar

--- OAuth2/Client/Impl/FacebookClient.cs
+++ OAuth2/Client/Impl/FacebookClient.cs
@@ -81,19 +81,19 @@ namespace OAuth2.Client.Impl
         protected override UserInfo ParseUserInfo(string content)
         {
             var response = JObject.Parse(content);
-            const string avatarUriTemplate = "{0}?type={1}";
-            var avatarUri = response["picture"]["data"]["url"].Value<string>();
+            const string avatarUriTemplate = "https://graph.facebook.com/{0}/picture?type={1}";
+            var id = response["id"].Value<string>();
             return new UserInfo
             {
-                Id = response["id"].Value<string>(),
+                Id = id,
                 FirstName = response["first_name"].Value<string>(),
                 LastName = response["last_name"].Value<string>(),
                 Email = response["email"].SafeGet(x => x.Value<string>()),
                 AvatarUri =
                 {
-                    Small = !string.IsNullOrWhiteSpace(avatarUri) ? string.Format(avatarUriTemplate, avatarUri, "small") : string.Empty,
-                    Normal = !string.IsNullOrWhiteSpace(avatarUri) ? string.Format(avatarUriTemplate, avatarUri, "normal") : string.Empty,
-                    Large = !string.IsNullOrWhiteSpace(avatarUri) ? string.Format(avatarUriTemplate, avatarUri, "large") : string.Empty
+                    Small = string.Format(avatarUriTemplate, id, "small"),
+                    Normal = string.Format(avatarUriTemplate, id, "normal"),
+                    Large = string.Format(avatarUriTemplate, id, "large")
                 }
             };
         }
